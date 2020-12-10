5ba3de6a1eec59cba2b4f9264c08687f2be73765
uTalDoTheo <60671364+uTalDoTheo@users.noreply.github.com>
2020-12-10 15:28:15 -0300

    conexão bd
    atualizado

--- qrcodeTheo/db.js
+++ qrcodeTheo/db.js
@@ -3,14 +3,14 @@ async function connect(){
      return global.connection;
 
 const mysql = require("mysql2/promise");
-const connection = await mysql.createConnection("mysql://app_qrcode:Senai115@DESKTOP-3NR8CSB:3306/CRUD");
+const connection = await mysql.createConnection("mysql://app_qrcode:Senai115@DESKTOP-3NR8CSB:3306/crud");
 console.log("Conectou no MySQL!");
 global.connection = connection;
 return connection;
 }
 connect();
 
-async function selecCustomers(){
+async function selecMaquinas(){
 const conn= await connect();
 const [rows] = conn.query('SELECT * FROM maquina')
 return await rows;
@@ -18,27 +18,27 @@ return await rows;
 
 
 }
-async function insertCustomer(maquina){
+async function insertMaquinas(maquina){
 
     const conn = await connect();
     const sql = 'INSERT INTO maquina(nome, manual , funcionamento) VALUES (?, ?, ?);';
     const values = [maquina.nome, maquina.manual, maquina.funcionamento];
-    await conn.query(sql, values);
+    return await conn.query(sql, values);
 }
 
-async function updateCustomer(id, maquina){
+async function updateMaquinas(id, maquina){
 
     const conn = await connect();
     const sql = 'UPDATE maquina SET nome=? , manual=?, funcionamento=?, id=?';
-    const values = [maquina.nome, maquina.manual, maquina.funcionamento];
+    const values = [maquina.nome, maquina.manual, maquina.funcionamento, id];
     return await conn.query(sql, values);
 }
-async function deleteCustomer(id){
+async function deleteMaquinas(id){
     const conn = await connect();
-    const sql = 'DELETE FROM maquina where id=?';
+    const sql = 'DELETE FROM maquina where id = ?;';
     return await conn.query(sql, [id]);
 }
 
 
-module.exports = {selectCustomers, insertCustomers, updateCustomers, deleteCustomers}
+module.exports = {selectMaquinas, insertMaquinas, updateMaquinas, deleteMaquinas}
 
