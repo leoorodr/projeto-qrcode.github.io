ede52f7f8d1c9b9b6ead52fc9cf1e130e7ca2a46
uTalDoTheo <60671364+uTalDoTheo@users.noreply.github.com>
2020-12-10 15:25:36 -0300

    Add files via upload

--- qrcodeTheo/index.js
+++ qrcodeTheo/index.js
@@ -5,21 +5,21 @@
     console.log('Começou!');
 
     console.log('INSERT INTO MAQUINA');
-    const result = await db.insertCustomer({nome: "Torno Schaublin", id:"1"})
+    const result = await db.insertMaquina({nome: "Torno Schaublin", id:"1"})
     console.log(result);
 
 
     console.log('SELECT *FROM MAQUINA');
-    const result = await db.selectCustomer({nome: "Torno Schaublin", id:"2"})
-    console.log(result);
+    const result = await db.selectMaquina({nome: "Tornno Schaublin", id:"1"})
+    console.log(maquina);
 
     console.log('UPDATE MAQUINA');
-    const result = await db.updateCustomer({nome: "Torno Schaublin", id:"3"})
-    console.log(result);
+    ult = await db.updateMaquina({nome: "Torno Schaublin", id:"1"})
+    console.log(result2);
 
     console.log('DELETE FROM MAQUINA');
-    const result = await db.deleteCustomer({nome: "Torno Schaublin", id:"4"})
-    console.log(result);
+    const result2 = await db.deleteMaquina({id:"1"})
+    console.log(result3);
 
   
   })();
