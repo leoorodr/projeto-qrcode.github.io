c55591b9fff37390b4a806ecab9f3f80e8d8241a
uTalDoTheo <60671364+uTalDoTheo@users.noreply.github.com>
2020-12-17 16:53:40 -0300

    qrcodeTheo COM CRUD
    Código atualizado com o crud

--- leoorodr.github.io-main/qrcodeTheo/db.js
+++ leoorodr.github.io-main/qrcodeTheo/db.js
@@ -1,9 +1,10 @@
+
 async function connect(){
     if(global.connection && global.connection.state !== 'disconnected' )
      return global.connection;
 
-const mysql = require("mysql2/promise");
-const connection = await mysql.createConnection("mysql://app_qrcode:Senai115@DESKTOP-3NR8CSB:3306/crud");
+const mysql = require("mysql/promise");
+const connection = await mysql.createConnection("mysql://app_qrcode:Senai115@qrcode.brazilsouth.cloudapp.azure.com:3306/db_qrcode");
 console.log("Conectou no MySQL!");
 global.connection = connection;
 return connection;
@@ -21,8 +22,8 @@ return await rows;
 async function insertMaquinas(maquina){
 
     const conn = await connect();
-    const sql = 'INSERT INTO maquina(nome, manual , funcionamento) VALUES (?, ?, ?);';
-    const values = [maquina.nome, maquina.manual, maquina.funcionamento];
+    const sql = 'INSERT INTO maquina(nome, manualMaquina ,manualFuncionamento, id ) VALUES (?, ?, ?);';
+    const values = [maquina.nome, maquina.manualMaquina, maquina.manualFuncionamento];
     return await conn.query(sql, values);
 }
 
